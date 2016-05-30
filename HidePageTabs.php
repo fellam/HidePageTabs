ca8efb9f2adfed786107b221ae06706735d82c03
Michele Fella <michele.fella@gmail.com>
2016-05-30 12:20:49 +0200

    debug

--- HidePageTabs.php
+++ HidePageTabs.php
@@ -130,12 +130,8 @@ function showTabsByGroupCategory($title, array &$links) {
 		if(count($showTabsByGroupCategory)>0){
 			$pageCategories=$wgOut->getCategories();
 			if(count($pageCategories)>0){
-				echo "<br> PAGE CATEGORIES:  <br>";
-				var_dump($pageCategories);
 				$userGroups=$wgUser->getGroups();
 				if(count($userGroups)>0){
-					echo "<br> USER GROUPS:  <br>";
-					var_dump($userGroups);
 					if(!in_array('sysop',$userGroups)
 						&&!in_array('smwadministrator',$userGroups)
 							){
@@ -160,21 +156,17 @@ function showTabsByGroupCategory($title, array &$links) {
 								}
 							}
 							if(count($hasGroupCategoryRights)>0){
-								echo "<br> SHOW:  <br>";
-								var_dump($hasGroupCategoryRights);
 								foreach ( $links as $group => $tabs ) {
-									echo "<br> GROUP: $group <br>";
-										switch ($group) {
-										case "views" :
-										case "actions" :
-											foreach ( $tabs as $tab => $props ) {
-												if (!in_array( $tab, $hasGroupCategoryRights)) {
-													echo "<br> GROUP HIDE: $tab <br>";
-													unset( $links[$group][$tab] );
-												}
+									switch ($group) {
+									case "views" :
+									case "actions" :
+										foreach ( $tabs as $tab => $props ) {
+											if (!in_array( $tab, $hasGroupCategoryRights)) {
+												unset( $links[$group][$tab] );
 											}
-											break;
 										}
+										break;
+									}
 								}
 							}else{
 								echo "<br> User has no allowed Group Category Show directive<br>";
@@ -189,7 +181,6 @@ function showTabsByGroupCategory($title, array &$links) {
 			}else{
 				echo "<br> Page has no CATEGORIES <br>";
 			}
-			exit();
 		}
 	}
 }
